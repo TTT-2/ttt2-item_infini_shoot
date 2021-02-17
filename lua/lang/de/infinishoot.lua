d46df7dae67213358f37e6376885808e500fca41
Tim Goll <tim@timgoll.de>
2021-02-17 11:29:31 +0100

    changed to new lang identifiers

--- lua/lang/de/infinishoot.lua
+++ lua/lang/de/infinishoot.lua
@@ -1,4 +1,4 @@
-local L = LANG.GetLanguageTableReference("deutsch")
+local L = LANG.GetLanguageTableReference("de")
 
 L["item_infini_shoot"] = "Unendlichschuss"
 L["item_infini_shoot_desc"] = "Mit Hilfe dieses Perks musst du deine Waffen nie wieder nachladen!"
--- lua/lang/en/infinishoot.lua
+++ lua/lang/en/infinishoot.lua
@@ -1,4 +1,4 @@
-local L = LANG.GetLanguageTableReference("english")
+local L = LANG.GetLanguageTableReference("en")
 
 L["item_infini_shoot"] = "Infini Shoot"
 L["item_infini_shoot_desc"] = "With the help of this perk you never have to reload your weapon again!"
