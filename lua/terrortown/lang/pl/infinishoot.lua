ea11af8e7dca7d0787b97938a501b5c747992527
lucapl <lukasz.andryszewski@outlook.com>
2024-09-03 00:48:38 +0200

    Added Polish translation

---
+++ lua/terrortown/lang/pl/infinishoot.lua
@@ -0,0 +1,4 @@
+local L = LANG.GetLanguageTableReference("pl")
+
+L["item_infini_shoot"] = "Nieskończone Strzelanie"
+L["item_infini_shoot_desc"] = "Dzięki temu już nigdy nie będziesz musiał przeładowywać swojej broni!"
